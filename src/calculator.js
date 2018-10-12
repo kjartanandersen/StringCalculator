6e37b94d3388ffded8eaa34db8beb1863bd3fb62
kjartanandersen <svidakjammi@gmail.com>
2018-10-12 23:20:11 +0000

    Implemented two numbers in string

--- src/calculator.js
+++ src/calculator.js
@@ -2,6 +2,10 @@ function add(number) {
 	if (number == "") {
 		return 0;
 	}
+	if (number.includes(",")) {
+		var numberArr = number.split(",");
+		return parseInt(numberArr[0]) + parseInt(numberArr[1]);
+	}
 	return parseInt(number);
 }
 
--- src/calculator.test.js
+++ src/calculator.test.js
@@ -7,3 +7,7 @@ it ("Should return zero on an empty string", () => {
 it ("Should return string if one number is in string", () => {
 	expect(add("5")).toBe(5);
 }); 
+
+it ("Should return sum of two given numbers", () => {
+	expect(add("1,2")).toBe(3);
+}); 
