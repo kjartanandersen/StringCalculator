b19bd86f53ae0d9268437dbd3a95dce0ef8bcf35
kjartanandersen <svidakjammi@gmail.com>
2018-10-12 23:13:38 +0000

    Did case if only one number in string

--- src/calculator.test.js
+++ src/calculator.test.js
@@ -4,3 +4,6 @@ it ("Should return zero on an empty string", () => {
 	expect(add("")).toBe(0);
 }); 
 
+it ("Should return string if one number is in string", () => {
+	expect(add("5")).toBe(5);
+}); 
